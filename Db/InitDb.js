50ec554f90da52d16428f5b319d2be7d64b80267
codeneno <qsmy_qin@163.com>
2021-10-04 17:20:28 +0800

    bat大致可用

--- Db/InitDb.js
+++ Db/InitDb.js
@@ -5,7 +5,7 @@ const { RegisterUser, Client, Tunnel, Connector } = require('./Models');
 const { v4: uuidv4 } = require('uuid');
 const logger = require('../Log/logger');
 const N2NServer = require('../N2N/N2NServer');
-const rootPath = require('./Common/GlobalData.js').rootPath;
+const rootPath = require('../Common/GlobalData.js').rootPath;
 let communityListPath = require('path').join(rootPath, 'config', 'community.list');
 /**
  * 初始化数据库
--- pkgbt.bat
+++ pkgbt.bat
@@ -1,4 +1,3 @@
-echo "delete dist files"
-
-del /f /s /q "dist\*.*"
-ncc build client.js  -o dist  && cd /d dist &&  pkg index.js -a 'config/**/*'  
+echo "delete dist files"  
+echo   %cd%
+del /f /s /q "dist\*.*"  &&  ncc build client.js  -o dist    && cd /d dist  &&  pkg index.js -a 'config/**/*'  && del /f /s /q "F:\nodeprjs\fastnat\dist\index.js"
