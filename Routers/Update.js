4f42999be42c2aa4bde871fea252e7949448c338
codeneno <qsmy_qin@163.com>
2021-12-06 10:47:28 +0800

    add ui update

--- Routers/Update.js
+++ Routers/Update.js
@@ -62,4 +62,42 @@ router.get('/api/check', async function(req, res, next) {
 });
 
 
+router.get('/api/uicheck', async function(req, res, next) {
+
+    let content = await readFile(serverConfigPath);
+    let updateConfig = JSON.parse(content).update;
+
+    //ui_linux_arm64_2.2.2.zip
+    let version = req.query.version;
+    let platform = req.query.platform;
+    let arch = req.query.arch;
+    let newVersion = updateConfig.uiVersion;
+    let baseUrl = updateConfig.baseUrl + '';
+
+    let pkgFileName = `ui_${platform}_${arch}_${newVersion}.zip`;
+
+    let canUpdate = compareVersions(newVersion, version) > 0;
+
+    if (baseUrl.startsWith('http') === false) {
+        let defaultWebSeverConfig = defaultConfig.webserver;
+        let serverPath = `http${defaultWebSeverConfig.https ? 's' : ''}://${defaultConfig.host}:${defaultWebSeverConfig.port}`;
+        baseUrl = `${serverPath+baseUrl}` + pkgFileName;
+    }
+
+    let data = {
+        canUpdate: canUpdate,
+        when: 'restart',
+        version: newVersion,
+        url: baseUrl,
+        des: updateConfig.des
+    };
+    logger.trace(data);
+    let result = {
+        success: true,
+        data: data,
+        info: ''
+    }
+    res.send(result);
+});
+
 module.exports = router
