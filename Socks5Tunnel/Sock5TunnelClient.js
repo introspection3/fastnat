8ebe913fdabe15d8b8595d9119113edfab5d8144
codeneno <qsmy_qin@163.com>
2022-05-13 15:01:22 +0800

    change socket.io socket.io@4.5.0 and @socket.io/cluster-adapter@0.2.0

--- Socks5Tunnel/Sock5TunnelClient.js
+++ Socks5Tunnel/Sock5TunnelClient.js
@@ -1,4 +1,3 @@
-
 const TcpTunnelClient = require('../TcpTunnel/TcpTunnelClient');
 const logger = require('../Log/logger');
 const socks5 = require('simple-socks');
@@ -26,9 +25,9 @@ class Sock5TunnelClient {
         }
 
     }
-    stop(){
+    stop() {
         this.socks5Server.close();
-        if(this.tcpTunnelClient){
+        if (this.tcpTunnelClient) {
             this.tcpTunnelClient.stop();
         }
     }
@@ -40,43 +39,48 @@ class Sock5TunnelClient {
         this.socks5Server = null;
         if (this.socks5Config.authenEnabled) {
             const options = {
-                authenticate:  (username, password, socket, callback)=> {
+                authenticate: (username, password, socket, callback) => {
                     if (username === this.socks5Config.username && password === this.socks5Config.password) {
                         return setImmediate(callback);
                     }
                     return setImmediate(callback, new Error('incorrect username and password'));
+                },
+                connectionFilter: function(destination, origin, callback) {
+                    console.log(destination.address);
+
+                    return setImmediate(callback);
                 }
             };
             this.socks5Server = socks5.createServer(options);
         } else {
             this.socks5Server = socks5.createServer();
-            
+
         }
-        this.socks5Server.on('authenticate',   (username)=> {
+        this.socks5Server.on('authenticate', (username) => {
             logger.debug('user ' + username + ' successfully authenticated!');
         });
-        this.socks5Server.on('authenticateError',   (username, err)=> {
+        this.socks5Server.on('authenticateError', (username, err) => {
             logger.error('user ' + username + ' failed to authenticate...' + err);
         });
-        this.socks5Server.on('handshake',   (socket)=> {
+        this.socks5Server.on('handshake', (socket) => {
             logger.trace('new socks5 client from ' + socket.remoteAddress + ':' + socket.remotePort);
         });
-        this.socks5Server.on('proxyError',   (err) =>{
+        this.socks5Server.on('proxyError', (err) => {
             logger.error('unable to connect to remote server');
             logger.error(err);
         });
-        this.socks5Server.on('proxyDisconnect',   (originInfo, destinationInfo, hadError)=> {
+        this.socks5Server.on('proxyDisconnect', (originInfo, destinationInfo, hadError) => {
             logger.debug(
                 `client ${originInfo.address}:${originInfo.port} request has disconnected from remote server at${destinationInfo.address}:${destinationInfo.port} with err ${hadError ? '' : 'no '}`);
         });
 
-        this.socks5Server.listen(this.tunnel.localPort, '0.0.0.0', async   () =>{
+        this.socks5Server.listen(this.tunnel.localPort, '0.0.0.0', async() => {
             logger.debug('SOCKS5 proxy server started on 0.0.0.0:' + this.tunnel.localPort);
-            this.tcpTunnelClient = new TcpTunnelClient(this.authenKey, this.tcpTunnelServerAddress, { port:  this.tunnel.localPort, host: '0.0.0.0' });
+            this.tcpTunnelClient = new TcpTunnelClient(this.authenKey, this.tcpTunnelServerAddress, { port: this.tunnel.localPort, host: '0.0.0.0' });
             await this.tcpTunnelClient.startTunnel(this.tunnel.id);
         });
     }
-    
+
 }
 
 module.exports = Sock5TunnelClient;
